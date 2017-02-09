67a02e04c2cd919718e809bc9f71bd0763a23c30
Metodos computacionales avanzados je.forero <j.e.forero.romero@gmail.com>
2017-02-09 10:01:11 -0500

    Prueba en cluster

---
+++ week3/submit_job.sh
@@ -0,0 +1,9 @@
+#PBS -N camilor_placas
+#PBS -l nodes=1:ppn=8
+#PBS -M ca.rivera965@uniandes.edu.co
+#PBS -m abe
+
+module load rocks-openmpi_ib
+cd $PBS_O_WORKDIR
+NPROCS=`wc -l < $PBS_NODEFILE`
+mpiexec -v -n $NPROCS ./placas
