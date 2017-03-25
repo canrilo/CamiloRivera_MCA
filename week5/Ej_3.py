f65bd7f00102ff0b264280c464d3412eb4a08b29
Camilo <canrilo_91@yahoo.com>
2017-03-24 19:22:20 -0500

    MCMC hamiltoniano

--- week5/Ej_3.py
+++ week5/Ej_3.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 
 #Solucion de Mac Cormak para ecuaciones de Euler en zonas de alta y baja presion
 
-def Mac_Cormack(gamma,U,F,dx,dt,tsteps):
+def Mac_Cormack(c,gamma,U,F,dx,dt, t_ini,t_fin):
 	U_temp=U.copy()
 	F_temp=F.copy()
 	
 	U_new=U.copy()
 	F_new=F.copy()
 	
-	for i in range(tsteps):
+	t=t_ini
+	
+	while t<t_fin:
+		print str(t)+':\n'
 		U_temp[:,1:-1]=U[:,1:-1]-dt*(F[:,2:]-F[:,1:-1])/dx
 		F_temp=get_F(U_temp,gamma)
 		
@@ -19,18 +22,30 @@ def Mac_Cormack(gamma,U,F,dx,dt,tsteps):
 		
 		F=F_new.copy()
 		U=U_new.copy()
-		#print i
-	return U
+		
+		umax=max(np.abs(safe_div(U[1,:],U[0,:])))
+		if umax!=0.0:
+			dt=max(1e-3,dx/(c*umax))
+		dt=min(dt,dx)
+
+		t+=dt
+		#print str(t)+': '+str(dt) +'\t '+str(umax)
+	return U 
 
 def get_F(U,gamma):
 	rho=U[0,:]
-	u=U[1,:]/rho
+	#u=U[1,:]/rho
+	u=safe_div(U[1,:],rho)
 	e=U[2,:]
 	p=(gamma-1)*(e-rho*(u**2)/2.0)
 	F=np.zeros(np.shape(U))
-	F[0,:]=u*rho
+	F[0,:]=U[1,:]
 	F[1,:]=rho*(u**2)+p
 	F[2,:]=u*(e+p)
+	print 'u:\n'+str(u)
+	print 'rho:\n'+str(rho)
+	print 'p:\n'+str(p)
+	raw_input()
 	return F
 
 def get_e(gamma, rho,u,p):
@@ -38,11 +53,18 @@ def get_e(gamma, rho,u,p):
 	e=p/(gamma-1)+rho*u2/(2.0)
 	return e
 
-dx=0.1
-dt=0.05
+def safe_div(num,denom):
+	ans=num.copy()
+	for i in range(len(num)):
+		if denom[i]!=0.0:
+			ans[i]=num[i]/denom[i]
+	return ans
+
+dx=0.05
+dt=0.00001
 tmax=1.0
+c=2.0
 xsteps=int(1/dx)+1
-tsteps=int(tmax/dt)+1
 gamma=1.4
 x=np.linspace(0,1,xsteps)
 u=np.zeros(np.shape(x))
@@ -67,10 +89,10 @@ F_ini[0,:]=u*rho
 F_ini[1,:]=rho*(u**2)+p
 F_ini[2,:]=u*(e+p)
 
-U_final=Mac_Cormack(gamma,U_ini,F_ini,dx,dt,tsteps)
+U_final=Mac_Cormack(c,gamma,U_ini,F_ini,dx,dt,0.0,tmax)
 
 rho_final =U_final[0,:]
-u_final = U_final[1,:]/rho_final
+u_final = safe_div(U_final[1,:],rho_final)#U_final[1,:]/rho_final
 e_final = U_final[2,:]
 p_final=(gamma-1)*(e_final-rho_final*(u_final**2)/2.0)
 
